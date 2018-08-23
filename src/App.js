5b43341287ee84044add51c089245502c5edd0f1
xingmengmeng <xingmengmeng@gomeholdings.com>
2018-08-23 17:54:46 +0800

    配置

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { observable } from "mobx";
 import './App.css';
 
+
+class Todo {
+    id = Math.random();
+    @observable title = "";
+    @observable finished = false;
+}
+
 class App extends Component {
+  componentDidMount(){
+
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        hello!
       </div>
     );
   }
